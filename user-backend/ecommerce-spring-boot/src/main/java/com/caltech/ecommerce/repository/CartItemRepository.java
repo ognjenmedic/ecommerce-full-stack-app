9a77559f1a5c01ce0e8a167a4bd936ed7bb68207
ognjenmedic <ognjen.medic@gmail.com>
2023-11-17 23:38:27 +0100

    added CartItemRepo

---
+++ user-backend/ecommerce-spring-boot/src/main/java/com/caltech/ecommerce/repository/CartItemRepository.java
@@ -0,0 +1,6 @@
+package com.caltech.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class CartItemRepository extends JpaRepository<CartItem, Long> {
+}
